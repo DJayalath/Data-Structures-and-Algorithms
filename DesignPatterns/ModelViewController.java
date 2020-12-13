1bcc79ca5380c9fee19f069dfaaa03e7fc675345
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-12-13 22:31:41 +0000

    Add a description of MVC pattern

---
+++ DesignPatterns/ModelViewController.java
@@ -0,0 +1,13 @@
+package DesignPatterns;
+
+// MVC pattern is mainly used for user interfaces (JavaFX uses MVC)
+
+// User -- USES --> Controller -- MANIPULATES --> Model -- UPDATES --> View -- SEEN BY -> User
+
+// The model is the application's dynamic data structure
+// The view is the presentation of the model as seen by the user
+// The controller responds to user input and manipulates the model
+
+public class ModelViewController {
+    
+}
