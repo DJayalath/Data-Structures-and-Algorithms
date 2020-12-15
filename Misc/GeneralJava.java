ff996b6c9ed6bd29c93de482efd80b455d3bffe2
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-12-15 09:55:41 +0000

    Add line by line reading

--- Misc/GeneralJava.java
+++ Misc/GeneralJava.java
@@ -1,10 +1,13 @@
 package Misc;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
 
 public class GeneralJava {
     public static void main(String[] args) throws Exception {
@@ -27,5 +30,13 @@ public class GeneralJava {
         byte b[] = new byte[1024];
         int length = in.read(b);
         in.close();
+
+        // Read line by line
+        BufferedReader reader = new BufferedReader(new FileReader(fin));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            // Process
+        }
+        reader.close();
     }
 }
